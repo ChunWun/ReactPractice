d3c8624ba74b7a2a4bc8ba82bb648fef1b8c567f
ChunWun <94582200+ChunWun@users.noreply.github.com>
2022-05-11 20:42:10 +0800

    add props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import ExpenseItem from "./components/ExpenseItem.js";
 
 function App() {
+
+	//TODO
+	const expenseItem = {
+		date: new Date(2022, 5, 11),
+		title: 'mask',
+		price: 50
+	}
+
 	return (
 		<div>
 			<h2>Let's get started!</h2>
-			<ExpenseItem />
+			<ExpenseItem date={expenseItem.date} title={expenseItem.title} price={expenseItem.price} />
 		</div>
 	);
 }
